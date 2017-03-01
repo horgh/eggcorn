c2eeb8baf5ec390c0e2c12763880b9b30416d541
Will Storey <will@summercat.com>
2017-03-01 12:36:05 -0800

    Add a test for the publish comment function

--- tests.js
+++ tests.js
@@ -15,7 +15,9 @@ const run_tests = function() {
 		failed++;
 	}
 
-	// TODO: publish_comment_to_sns
+	if (!test_publish_comment_to_sns()) {
+		failed++;
+	}
 
 	if (!test_make_error_html()) {
 		failed++;
@@ -35,6 +37,134 @@ const run_tests = function() {
 	}
 
 	console.log("All tests passed.");
+	return true;
+};
+
+const test_publish_comment_to_sns = function() {
+	const tests = [
+		{
+			sns_arn: 'test:arn',
+			comment: {
+				name:  'John Public',
+				email: 'john@example.com',
+				text:  'Nice site',
+				url:   'https://www.example.com',
+				ip:    '127.0.0.1',
+				time:  123,
+				id:    'aaa-bbb'
+			},
+			output: {
+				TopicArn: 'test:arn',
+				Message:  "New comment from John Public:\nNice site",
+				Subject:  'New comment',
+				MessageAttributes: {
+					name:  {StringValue: 'John Public'},
+					email: {StringValue: 'john@example.com'},
+					text:  {StringValue: 'Nice site'},
+					url:   {StringValue: 'https://www.example.com'},
+					ip:    {StringValue: '127.0.0.1'},
+					time:  {StringValue: '123'},
+					id:    {StringValue: 'aaa-bbb'}
+				}
+			}
+		}
+	];
+
+	// Dummy SNS objects. They just make the published object accessible for us
+	// to check, and call the callback indicating an error or not.
+
+	let published_object = null;
+
+	const dummy_sns_that_succeeds = {
+		publish: function(o, callback) {
+			published_object = o;
+			callback(null, {});
+		}
+	};
+
+	const dummy_sns_that_fails = {
+		publish: function(o, callback) {
+			published_object = o;
+			callback('out of memory', {});
+		}
+	};
+
+	// Callback that sns.publish() calls.
+	let callback_error = null;
+	let callback_comment = null;
+	const dummy_callback = function(err, comment) {
+		callback_error = err;
+		callback_comment = comment;
+	};
+
+	let failed = 0;
+	test_loop:
+	for (let i = 0; i < tests.length; i++) {
+		const test = tests[i];
+
+		functions.publish_comment_to_sns(dummy_sns_that_succeeds, test.sns_arn,
+			test.comment, dummy_callback);
+
+		const top_props = ['TopicArn', 'Message', 'Subject'];
+		for (let j = 0; j < top_props.length; j++) {
+			const prop = top_props[j];
+
+			if (published_object[prop] !== test.output[prop]) {
+				console.log("publish_comment_to_sns " + prop + " = " +
+					published_object[prop] + ", wanted " + test.output[prop]);
+				failed++;
+				break test_loop;
+			}
+		}
+
+		const attributes = ['name', 'email', 'text', 'url', 'ip', 'time', 'id'];
+		for (let j = 0; j < attributes.length; j++) {
+			const prop = attributes[j];
+
+			if (published_object.MessageAttributes[prop].StringValue !==
+				test.output.MessageAttributes[prop].StringValue) {
+				console.log("publish_comment_to_sns " + prop + " = " +
+					published_object.MessageAttributes[prop].StringValue + ", wanted " +
+					test.output.MessageAttributes[prop].StringValue);
+				failed++;
+				break test_loop;
+			}
+		}
+
+		if (callback_error !== null) {
+			console.log("publish_comment_to_sns: success callback resulted in error?");
+			failed++;
+			continue;
+		}
+
+		// The comment should be the same. I just check a single field right now.
+		if (callback_comment.name !== test.comment.name) {
+			console.log("publish_comment_to_sns: success callback comment mismatch");
+			failed++;
+			continue;
+		}
+
+		// Call with a dummy SNS that fails.
+		//
+		// We don't need to check the published object again. It's the same in
+		// either case. Check that we received an error though.
+
+		functions.publish_comment_to_sns(dummy_sns_that_fails, test.sns_arn,
+			test.comment, dummy_callback);
+
+		if (callback_error !== 'Unable to publish comment') {
+			console.log("publish_comment_to_sns: error callback had no error?");
+			failed++;
+			continue;
+		}
+	}
+
+	if (failed > 0) {
+		console.log("publish_comment_to_sns: " + failed + "/" + tests.length +
+			" tests failed!");
+		return false;
+	}
+
 	return true;
 };
 
